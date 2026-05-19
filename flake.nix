63ae2986586b4194181ee60ba72e7dd8057b6b77
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-05-19 10:55:12 -0700

    ci(flake): improve flake.nix
    improve overlay creation

--- flake.nix
+++ flake.nix
@@ -14,58 +14,52 @@
         overlays = (if builtins.isList overlays then overlays else []) ++ inputs.pkgs.overlays or [];
         config = inputs.pkgs.config or {};
       };
-    mapAttrsToList = f: attrs: builtins.attrValues (builtins.mapAttrs f attrs);
-    l_pkg_enum = {
-      lua5_1 = "lua51Packages";
-      lua5_2 = "lua52Packages";
-      lua5_3 = "lua53Packages";
-      lua5_4 = "lua54Packages";
-      lua5_5 = "lua55Packages";
-      luajit = "luajitPackages";
-      lua = "luaPackages";
-    };
     APPNAME = "osenv";
-    overlay = final: prev: let
-      luaCallPackageFn = { buildLuarocksPackage, }:
-        buildLuarocksPackage {
-          pname = APPNAME;
-          version = "scm-1";
-          src = self;
-          checkPhase = ''
-            runHook preCheck
-            runHook postCheck
-          '';
-          installCheckPhase = ''
-            runHook preInstallCheck
-            luarocks test
-            runHook postInstallCheck
-          '';
-          meta = {
-            maintainers = [ lib.maintainers.birdee ];
-            license = lib.licenses.mit;
-          };
-        };
-      # lua5_1 = prev.lua5_1.override { packageOverrides };
-      l_pkg_main = builtins.mapAttrs (
-        n: _: (prev.lib.attrByPath [ n "override" ] null prev) (old: {
-          packageOverrides = luaself: luaprev: (
-            (if old ? packageOverrides then old.packageOverrides luaself luaprev else {}) // {
-              ${APPNAME} = luaself.callPackage luaCallPackageFn {};
-            }
-          );
-        })
-      ) l_pkg_enum;
-      # lua51Packages = final.lua5_1.pkgs;
-      l_pkg_sets = builtins.listToAttrs (
-        mapAttrsToList (
-          n: v: {
-            name = v;
-            value = prev.lib.attrByPath [ n "pkgs" ] null final;
+    l_pkg_enum = [ "lua5_1" "lua5_2" "lua5_3" "lua5_4" "lua5_5" "luajit" "lua" ];
+    mkLuaOverlay = { packageOverrides, vimPlugins ? null, versions ? [], controlType ? "exclude", ... }:
+      assert builtins.isList versions || throw "lua versions must be a list of strings containing `lua.luaAttr` names corresponding to `pkgs.luaInterpreters`!";
+      assert controlType == "build" || controlType == "exclude" || throw ''controlType must be "build" or "exclude"'';
+    final: prev: {
+      luaInterpreters = prev.luaInterpreters // prev.lib.pipe (
+        if controlType == "build" then
+          prev.lib.intersectLists versions (builtins.attrNames prev.luaInterpreters)
+        else
+          builtins.filter (x: !builtins.elem x versions) (builtins.attrNames prev.luaInterpreters)
+      ) [
+        (map (v: prev.lib.nameValuePair v packageOverrides))
+        builtins.listToAttrs
+        (builtins.mapAttrs (
+          n: new: prev.luaInterpreters.${n}.override (old: {
+            packageOverrides = prev.lib.composeExtensions (old.packageOverrides or (_: _: {})) new;
+          })
+        ))
+      ];
+      ${if prev.lib.isFunction vimPlugins then "vimPlugins" else null} = prev.vimPlugins // vimPlugins final prev;
+    };
+    overlay = mkLuaOverlay {
+      packageOverrides = luaself: luaprev: {
+        ${APPNAME} = luaself.callPackage (
+          { buildLuarocksPackage, }: buildLuarocksPackage {
+            pname = APPNAME;
+            version = "scm-1";
+            src = self;
+            checkPhase = ''
+              runHook preCheck
+              runHook postCheck
+            '';
+            installCheckPhase = ''
+              runHook preInstallCheck
+              luarocks test
+              runHook postInstallCheck
+            '';
+            meta = {
+              maintainers = [ lib.maintainers.birdee ];
+              license = lib.licenses.mit;
+            };
           }
-        ) l_pkg_enum
-      );
-    in l_pkg_main // l_pkg_sets // {
-      vimPlugins = prev.vimPlugins // {
+        ) {};
+      };
+      vimPlugins = final: prev: {
         ${APPNAME} = final.neovimUtils.buildNeovimPlugin {
           pname = APPNAME;
           version = "master";
@@ -78,11 +72,11 @@
     packages = forAllSys (system: let
       pkgs = getPkgs system [ overlay ];
     in (
-      with builtins; listToAttrs (
+      builtins.listToAttrs (
         map (n: {
           name = "${n}-${APPNAME}";
           value = pkgs.lib.attrByPath [ n "pkgs" APPNAME ] null pkgs;
-        }) (attrNames l_pkg_enum)
+        }) l_pkg_enum
       )
     ) // {
       default = pkgs.vimPlugins.${APPNAME};
@@ -95,6 +89,7 @@
     devShells = forAllSys (system: let
       pkgs = getPkgs system [];
       lua = pkgs.luajit.withPackages (lp: [ lp.inspect lp.luarocks ]);
+    in {
       default = pkgs.mkShell {
         name = "${APPNAME}-dev";
         packages = [ lua ];
@@ -106,19 +101,12 @@
           if [ -n "$gitdir" ]; then
             export PREFIX="$gitdir/build/test"
             cd "$gitdir"
-            make clean bear
+            make bear
             cd "$ogdir"
           fi
           unset gitdir ogdir
         '';
       };
-    in {
-      inherit default;
-      zsh = default.overrideAttrs (old: {
-        shellHook = old.shellHook + ''
-          exec zsh
-        '';
-      });
     });
   };
 }
